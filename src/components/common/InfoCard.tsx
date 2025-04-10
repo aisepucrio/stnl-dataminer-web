71e0bf758b58168155e4412895b9fc788746dc03
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2025-04-10 19:03:54 -0300

    Update InfoCard.tsx

--- src/components/common/InfoCard.tsx
+++ src/components/common/InfoCard.tsx
@@ -1,71 +1,48 @@
-'use client'
+"use client";
 
-import { Box, Typography, Skeleton } from '@mui/material';
+import { Box, Typography, Skeleton } from "@mui/material";
 
 type InfoCardProps = {
   label: string;
-  value: number;
+  value: number | null;
   isLoading?: boolean;
 };
 
 const InfoCard = ({ label, value, isLoading = false }: InfoCardProps) => {
   return (
-    <Box
-      sx={{
-        width: 1446,
-        height: 1020,
-        borderRadius: '50px',
-        bgcolor: 'white',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: 3,
-      }}
-    >
+    <>
       {isLoading ? (
         <>
-          <Skeleton variant="rectangular" width={1016} height={187} />
-          <Skeleton
-            variant="rectangular"
-            width={459}
-            height={275}
-            sx={{ mt: 4 }}
-          />
+          <Skeleton variant="rectangular" height={"16vh"} sx={{flex:1}}/>
         </>
       ) : (
         <>
           <Box
             sx={{
-              width: 1016,
-              height: 187,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              //   width: "100%",
+              flex: 1,
+              height: "16vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              bgcolor: "pink",
+              boxSizing: "border-box",
+              px: "0",
+              py: "50px",
             }}
           >
             <Typography variant="h4" color="text.secondary">
               {label}
             </Typography>
-          </Box>
 
-          <Box
-            sx={{
-              width: 459,
-              height: 275,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              mt: 4,
-            }}
-          >
             <Typography variant="h2" color="text.primary">
               {value}
             </Typography>
           </Box>
         </>
       )}
-    </Box>
+    </>
   );
 };
 
