f22f912e96a31da8616432e7ae41b84114eeaa10
Juliana Pinheiro dos Santos <pinheiroju00@gmail.com>
2025-04-14 11:20:27 -0300

    feat/customize-menu-bar

--- src/components/layout/MainMenu.tsx
+++ src/components/layout/MainMenu.tsx
@@ -1,22 +1,17 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import {
-  AppBar,
-  Container,
   Box,
   Toolbar,
   Typography,
-  Menu,
-  MenuItem,
-  IconButton,
-  Button,
-  Tooltip,
-  Avatar,
+  List,
+  Drawer,
+  ListItemButton,
+  ListItem,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import AdbIcon from "@mui/icons-material/Adb";
 
 const pages = [
   { name: "Dashboard", path: "/" },
@@ -25,115 +20,63 @@ const pages = [
   { name: "Jobs", path: "/jobs" },
 ];
 
-const settings = [
-  { name: "Profile", path: "/profile" },
-  { name: "Account", path: "/account" },
-  { name: "Dashboard", path: "/dashboard" },
-  { name: "Logout", path: "/logout" },
-];
-
 const MainMenu = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  const pathname = usePathname();
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
+    <Box sx={{ display: "flex" }}>
+      
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          padding: 10,
+          "& .MuiDrawer-paper": {
+            width: 240,
+            boxSizing: "border-box",
+            backgroundColor: "#ffffff",
 
-          {/* Menu Mobile */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton onClick={handleOpenNavMenu} color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorElNav}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                  <Link href={page.path} passHref style={{ textDecoration: "none", color: "inherit" }}>
-                    <Typography>{page.name}</Typography>
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          },
+        }}
+        >
+        <Toolbar />
+        
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {pages.map(({ name, path }) => {
+              const isActive = pathname === path;
 
-          {/* Menu Desktop */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button key={page.name} color="inherit" component={Link} href={page.path}>
-                {page.name}
-              </Button>
-            ))}
-          </Box>
-
-          {/* Menu Usuário */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {/* <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" /> */}
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={anchorElUser}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-              sx={{ mt: "45px" }}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
-                  <Link href={setting.path} passHref style={{ textDecoration: "none", color: "inherit" }}>
-                    <Typography>{setting.name}</Typography>
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              return (
+                <ListItem disablePadding>
+                  <ListItemButton
+                    component="a"
+                    sx={{
+                      backgroundColor: isActive ? "#1C488638" : "transparent",
+                      borderRadius: 2,
+                      margin:1.5,
+                    }}
+                  >
+                    <Typography
+                    sx={{
+                      fontSize: 18,
+                      fontWeight: 800,
+                      color: isActive ? "#1C4886" : "#BCBDBC",
+                    }}
+                    >
+                    {name}
+                  </Typography>  
+          
+                </ListItemButton>
+                </ListItem>
+                
+              );
+            })}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
   );
 };
 
