22927ffb1eadf85a3cb4d4e9909353ef3fe063d0
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2025-05-07 10:31:31 -0300

    Update page.tsx

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,52 @@
-import { Box } from "@mui/material";
+"use client"
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography , Paper} from "@mui/material";
+import { useEffect, useState } from "react";
 
-const Jobs = () => {
-    return <Box>this is a Jobs</Box>;
+type Job = {
+  task_id: string;
+  operation: string;
+  repository: string;
+  created_at: string;
+  created_at_formatted: string;
+  status: string;
+  error: string;
 };
 
+const Jobs = () => {
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/jobs/")
+      .then((res) => res.json())
+      .then(setJobs)
+      .catch(console.error);
+  }, []);
+
+  return  <Box p={3}>
+  <Typography variant="h4" gutterBottom>
+    Jobs
+  </Typography>
+
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>ID</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Operation</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {jobs.map((job) => (
+          <TableRow key={job.task_id}>
+            <TableCell>{job.task_id}</TableCell>
+            <TableCell>{job.status}</TableCell>
+            <TableCell>{job.operation}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+</Box>
+}
 export default Jobs;
