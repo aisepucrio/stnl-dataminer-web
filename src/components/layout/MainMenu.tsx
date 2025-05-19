929c000907c891e9e8a45e2366449605d777d51d
Juliana Pinheiro dos Santos <pinheiroju00@gmail.com>
2025-05-19 09:51:37 -0300

    fix : join branches ( menu )

--- src/components/layout/MainMenu.tsx
+++ src/components/layout/MainMenu.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useSearchParams } from "next/navigation";
-
 import {
   Box,
   List,
@@ -12,12 +11,12 @@ import {
   ListItemButton,
   ListItem,
   ListItemText,
-  ListItemIcon, 
-  IconButton,
+  ListItemIcon,
   Icon,
-  Button,
 } from "@mui/material";
 import SourceSwitcher from "../ui/SourceSwitcher";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/app/store";
 
 // import icons 
 import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
@@ -27,256 +26,189 @@ import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 
-
-const pages = [
-  { name: "Overview", path: "/", icon: <QueryStatsOutlinedIcon sx={{fontSize:22}}></QueryStatsOutlinedIcon>},
-  { name: "Preview", path: "/preview", icon: <WorkOutlineOutlinedIcon sx={{fontSize:22}}></WorkOutlineOutlinedIcon>, children:[],},
-  { name: "Collect", path: "/collect", icon: <FolderOutlinedIcon sx={{fontSize:22}}></FolderOutlinedIcon> },
-  { name: "Jobs", path: "/jobs", icon: <ImportContactsOutlinedIcon sx={{fontSize:22}}></ImportContactsOutlinedIcon>},
-];
-
-// if preview, tenho que colocar mais um icone 
-
-
 const MainMenu = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const source = useSelector((state: RootState) => state.source.value);
 
+  const [previewOpen, setPreviewOpen] = useState(false);
 
-
-  const [selectedOption, setSelectedOption] = useState("GitHub");
-  const [optionsOpen, setOptionsOpen] = useState(false);
-
-
-  const getPreviewSubItems = () => {
-    if(selectedOption === "GitHub"){
-      return[
-        {name: "Issue"}, 
-        {name: "Pull Request"},
-        {name: "Comment"}, 
-        {name: "Commit"}, 
-      ];
-    } else if(selectedOption === "Jira"){
-      return[
-        {name: "Issue"}, 
-        {name: "Users"},
-        {name: "Comment"},
-        {name: "Commit"},
-        {name: "Pull Request"},
-      ];
+  const getPreviewChildren = () => {
+    switch (source) {
+      case "github":
+        return [
+          { title: "Repository", to: "/preview/github/repository" },
+          { title: "Issue", to: "/preview/github/issue" },
+          { title: "Pull Request", to: "/preview/github/pullrequest" },
+          { title: "Comment", to: "/preview/github/comment" },
+          { title: "Commit", to: "/preview/github/commit" },
+        ];
+      case "jira":
+        return [
+          { title: "Users", to: "/preview/jira/users" },
+          { title: "Issue", to: "/preview/jira/issue" },
+          { title: "Pull Request", to: "/preview/jira/pullrequest" },
+          { title: "Comment", to: "/preview/jira/comment" },
+          { title: "Commit", to: "/preview/jira/commit" },
+        ];
+      default:
+        return null;
     }
+  };
 
-  }
-
-  const pagesItems = pages.map(item =>{ 
-    if(item.name === "Preview"){
-      return {
-        ... item, children: getPreviewSubItems() 
-      };
-      return item;
-    }
-
-  });
+  const pages = [
+    { name: "Overview", path: "/", icon: <QueryStatsOutlinedIcon sx={{ fontSize: 22 }} /> },
+    {
+      name: "Preview",
+      path: "/preview",
+      icon: <WorkOutlineOutlinedIcon sx={{ fontSize: 22 }} />,
+      children: getPreviewChildren(),
+    },
+    { name: "Collect", path: "/collect", icon: <FolderOutlinedIcon sx={{ fontSize: 22 }} /> },
+    { name: "Jobs", path: "/jobs", icon: <ImportContactsOutlinedIcon sx={{ fontSize: 22 }} /> },
+  ];
 
   return (
     <Box sx={{ display: "flex" }}>
-      
       <Drawer
         variant="permanent"
         anchor="left"
         sx={{
-          
           width: 240,
           flexShrink: 0,
           "& .MuiDrawer-paper": {
             width: 240,
             boxSizing: "border-box",
             backgroundColor: "#ffffff",
-            display: "flex", 
-            justifyContent: "space-between", 
-            flexDirection: "column"
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
           },
         }}
       >
-        
-        <Box sx={{position: "relative" }}> 
-          <Button 
-          variant="contained"
-          onClick={() => setOptionsOpen(!optionsOpen)}
-          sx={{
-            backgroundColor: "#1C4886",
-            color : "#FFFFFF",
-            fontSize: 18,
-            width: 150, 
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center", 
-            textAlign: "center",
-            borderRadius: 3,
-            padding: 1,
-            marginTop: 5,
-            marginLeft: 4,
-          }}
-          startIcon={ optionsOpen ? <ExpandMoreRoundedIcon/> : <ChevronRightOutlinedIcon/>}
-          >
-            {selectedOption}
-          </Button>
-
-          {optionsOpen && (
-          <Box
-            sx={{
-              position: "absolute", 
-              top: "100%", 
-              left: 0, 
-              width: 150, 
-              boxShadow: 3, 
-              borderRadius: 1, 
-              zIndex: 10,
-              marginLeft: 4,
-              backgroundColor: "white"
-
-            }}
-            >
-            {["GitHub", "Jira"].map((option) => (
-              <Box
-              key={option}
-              onClick ={() => {
-                setSelectedOption(option);
-                setOptionsOpen(false);
-              }}
-              sx={{
-                padding: .5,
-                cursor: "pointer",
-                border: "0.5px solid #1C1C1C33",
-                textAlign: "center"
-                                  
-              }}
-              >
-                {option}
-              </Box>
-            ))}
-
-          </Box>
-          )}
-          
+        <Box 
+        sx={{
+          mt: 7,
+          mx: 2,
+        }}>
+          <SourceSwitcher />
         </Box>
-          <SourceSwitcher/>
-        <Box sx={{ overflow: "auto", flexGrow : 1}}>
+        
+
+        <Box sx={{ overflow: "auto", flexGrow: 1 }}>
           <List sx={{ marginTop: 10 }}>
             {pages.map(({ name, path, icon, children }) => {
-                const isActive = pathname === path;
-                if(name === "Preview"){
-                  const subItems = getPreviewSubItems();
+              const isActive = pathname === path;
 
-                  return(
-                    <Link href={path} key={name} passHref legacyBehavior>  
-                      <ListItem disablePadding sx={{
-                        alignItems: "center",
-                        justifyContent: "center"
-                      }}>
-                        <Icon sx={{
-                          paddingLeft: 1,
-                        }}> 
-                          <ChevronRightOutlinedIcon sx={{
-                            fontSize: 24,
-                          }}></ChevronRightOutlinedIcon>
+              const isPreview = name === "Preview";
+
+              return (
+                <React.Fragment key={name}>
+                  <ListItem disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        if (isPreview) {
+                          setPreviewOpen(!previewOpen);
+                        }
+                      }}
+                      component={!isPreview ? "a" : "div"}
+                      href={!isPreview ? path : undefined}
+                      sx={{
+                        width: "80%",
+                        borderRadius: 2,
+                        margin: 1,
+                        ml: isPreview ? 0 : 3,
+                        mr: 2,
+                        display: "flex",
+                        "&:hover": {
+                          backgroundColor: "action.hover",
+                        },
+                      }}
+                    >
+                      {isPreview && (
+                        <Icon>
+                          {previewOpen ? <ExpandMoreRoundedIcon /> : <ChevronRightOutlinedIcon />}
                         </Icon>
-                        <ListItemButton 
-                        component="a"
-                        sx={{
-                          width: "80%",
-                          borderRadius: 2,
-                          marginTop: 1,
-                          marginBottom: 1,
-                          marginRight: 2,
-                          display: "flex",
-                          "&:hover": {
-                            backgroundColor: "action.hover"
-                          },
-                        }}>
-                        <ListItemIcon sx={{ fontSize: 24 }}>
-                          {icon} </ListItemIcon>
-                        <ListItemText 
-                        primary={name} 
+                      )}
+                      <ListItemIcon sx={{ fontSize: 24 }}>{icon}</ListItemIcon>
+                      <ListItemText
+                        primary={name}
                         primaryTypographyProps={{
                           fontWeight: 500,
                           fontSize: 20,
-                          color: "#000000"
-                        }}/>
-
-
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                  )
-                }
-
-                return (
-                  <Link href={path} key={name} passHref legacyBehavior>
-                    <ListItem disablePadding>
-                      <ListItemButton
-                        component="a"
-                        sx={{
-                          width: "80%",
-                          borderRadius: 2,
-                          margin: 1,
-                          marginLeft: 3,
-                          marginRight: 2,
-                          display: "flex",
-                          "&:hover": {
-                            backgroundColor: "action.hover"
-                          },
+                          color: "#000000",
                         }}
-                      >
-                        <ListItemIcon sx={{ fontSize: 24 }}>
-                           {icon} </ListItemIcon>
-                        <ListItemText 
-                        primary={name} 
-                        primaryTypographyProps={{
-                          fontWeight: 500,
-                          fontSize: 20,
-                          color: "#000000"
-                        }}/>
-                      </ListItemButton>
-                    </ListItem>
-                  </Link>
-                );
-            })}
+                      />
+                      
+                    </ListItemButton>
+                  </ListItem>
 
+                  {/* Renderiza os filhos do Preview */}
+                  {isPreview && previewOpen && children && (
+                    <Box sx={{ ml: 5 }}>
+                      {children.map((child) => (
+                        <Link href={child.to} key={child.title} passHref legacyBehavior>
+                          <ListItem disablePadding>
+                            <ListItemButton
+                              component="a"
+                              sx={{
+                                width: "100%",
+                                borderRadius: 2,
+                                marginY: 0.5,
+                                pl: 2,
+                                "&:hover": {
+                                  backgroundColor: "action.hover",
+                                },
+                              }}
+                            >
+                              <ListItemText
+                                primary={child.title}
+                                primaryTypographyProps={{
+                                  fontSize: 16,
+                                  color: "#333",
+                                }}
+                              />
+                            </ListItemButton>
+                          </ListItem>
+                        </Link>
+                      ))}
+                    </Box>
+                  )}
+                </React.Fragment>
+              );
+            })}
           </List>
         </Box>
-        <Box
-          sx={{ display: "flex", justifyContent: "center" }}
-          >
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
           <Image
             src="/images/logo-raise.svg"
             alt="Logo"
             width={170}
             height={70}
-            style={{ 
-              objectFit: "contain", 
+            style={{
+              objectFit: "contain",
               marginTop: 15,
             }}
           />
-          
         </Box>
-        <Box 
-        component="a"
-        href={apiUrl}
-        sx={{
-          border: "3px solid #1C4886",
-          fontSize: 18,
-          fontWeight: 600,
-          color: "#1C4886",
-          borderRadius: 2,
-          margin: 1.5,
-          textAlign: "center", 
-          paddingTop: "7px",
-          paddingBottom: "7px", 
-          marginBottom: 5,
-          textDecoration: "none"
-        }}>
+
+        <Box
+          component="a"
+          href={apiUrl}
+          sx={{
+            border: "3px solid #1C4886",
+            fontSize: 18,
+            fontWeight: 600,
+            color: "#1C4886",
+            borderRadius: 2,
+            margin: 1.5,
+            textAlign: "center",
+            paddingY: "7px",
+            marginBottom: 5,
+            textDecoration: "none",
+          }}
+        >
           View API Docs
         </Box>
       </Drawer>
