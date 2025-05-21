98df7e46e8e0aa4ebda2e762ab2b864310236d7c
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2025-05-21 17:03:21 -0300

    feat: Removing color test

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -5,8 +5,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     background: {
-      // default: '#fff', // cor de fundo padrão
-      default: "green"
+      default: '#fff', // cor de fundo padrão
     },
   },
 });
