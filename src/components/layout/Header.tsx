299b4c40c7abd1d938b54a382745c483e7592d8c
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2025-05-28 11:26:38 -0300

    fix: Correcting description of route
    also, the componend was changed from Box and Typography to a Breadcrumbs component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Breadcrumbs, Link } from "@mui/material";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import ItemSwitcher from "../ui/itemSwitcher";
@@ -11,15 +11,16 @@ const row = {
   alignItems: "center",
 };
 
-const formatPathName = (path: string) => {
-  const last = path.split("/").filter(Boolean).pop();
-  if (!last) return "Dashboard";
-  return last.charAt(0).toUpperCase() + last.slice(1);
-};
+const formatSegment = (segment: string) =>
+  segment.charAt(0).toUpperCase() + segment.slice(1);
 
 const Header = () => {
   const pathname = usePathname();
-  const pageTitle = formatPathName(pathname);
+  const pathSegments = pathname.split("/").filter(Boolean);
+
+  const breadcrumbs = pathSegments.length === 0
+    ? ["Overview"]
+    : pathSegments.map(formatSegment);
 
   return (
     <Box
@@ -36,40 +37,34 @@ const Header = () => {
         sx={{
           height: "28px",
           width: "256px",
-          bgcolor: "",
           alignItems: "center",
           ...row,
         }}
       >
-        <Box sx={{ ...row, width: "184px", px: "8px" }}>
-          <Typography
-            sx={{ color: "#a0a2a4", fontSize: "16px" }}
-            fontWeight={400}
-          >
-            {pageTitle}
-          </Typography>
-          <Typography>/</Typography>
-          <Typography>Default</Typography>
-        </Box>
+        <Breadcrumbs aria-label="breadcrumb">
+          {breadcrumbs.map((crumb, idx) => (
+            <Typography key={idx} sx={{ color: "#a0a2a4", fontSize: "16px" }}>
+              {crumb}
+            </Typography>
+          ))}
+        </Breadcrumbs>
       </Box>
 
-      <Box sx={{ width: "180px", bgcolor: "", ...row, height: "40px" }}>
-        {/* sdf */}
+      <Box sx={{ width: "180px", ...row, height: "40px" }}>
         <ItemSwitcher />
       </Box>
+
       <Box
         sx={{
           height: "28px",
           width: "180px",
-          bgcolor: "",
           ...row,
-
           justifyContent: "flex-end",
         }}
       >
         <Image
           src="/icons/themeMode.svg"
-          alt="Sidebar"
+          alt="Theme Mode"
           width={25}
           height={25}
         />
