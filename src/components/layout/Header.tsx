815c85e09e59c93b968c8fa4ebf4681b18079621
Juliana Pinheiro dos Santos <pinheiroju00@gmail.com>
2025-06-12 09:03:46 -0300

    feat: changing export button to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, Typography, Button, Breadcrumbs, Link, Tooltip } from "@mui/material";
+import { Box, Typography, Button, Breadcrumbs, Link, Tooltip, FormControl, Dialog, DialogContent, MenuItem, Select, DialogActions, SelectChangeEvent } from "@mui/material";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import ItemSwitcher from "../ui/itemSwitcher";
 import ArticleIcon from '@mui/icons-material/Article';
+import { useState } from "react";
 
 const row = {
   display: "flex",
@@ -18,11 +19,37 @@ const formatSegment = (segment: string) =>
 const Header = () => {
   const pathname = usePathname();
   const pathSegments = pathname.split("/").filter(Boolean);
+  const isPreviewPage = pathname.includes("/preview");
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedFormat, setSelectedFormat] = useState<string>('');
+
 
   const breadcrumbs = pathSegments.length === 0
     ? ["Overview"]
     : pathSegments.map(formatSegment);
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setSelectedFormat('');
+  };
+
+  const handleFormatChange = (event: SelectChangeEvent) => {
+    setSelectedFormat(event.target.value as string);
+  };
+
+  const handleExport = () => {
+    if (selectedFormat) {
+      console.log(`Exportando no formato: ${selectedFormat}`);
+      handleCloseDialog();
+    }
+  };
 
   return (
     <Box
@@ -51,6 +78,96 @@ const Header = () => {
           ))}
         </Breadcrumbs>
       </Box>
+      
+      {isPreviewPage &&
+      <Box
+          sx={{
+            width: "13vw",
+            
+            display: "flex",
+            justifyContent: "flex-start",
+            backgroundColor: "transparent",
+            
+          }}
+          >
+          <Button
+            onClick={handleOpenDialog} 
+            variant="outlined"
+            sx={{
+              border: "0.15rem solid #1C4886",
+              color: "#1C4886",
+              borderRadius: "16px",
+              marginLeft: "-10vw",
+              padding: "14px 16px",
+              textTransform: "none",
+              fontSize: "1rem",
+              fontWeight: 600,
+              "&:hover": {
+                backgroundColor: "rgba(28, 72, 134, 0.04)",
+                borderColor: "#1C4886",
+              },
+            }}>
+
+            Export CSV / JSON
+          </Button>
+          <Dialog open={openDialog} 
+      onClose={handleCloseDialog}
+      PaperProps={{
+        sx: {
+          bgcolor: "#21211F", 
+          borderRadius: '16px', 
+        },
+      }}>        
+        <DialogContent> 
+          <FormControl fullWidth sx={{ minWidth: 400}}>
+            <Select
+              labelId="format-select-label"
+              id="format-select"
+              value={selectedFormat}
+              onChange={handleFormatChange}
+              sx={{
+                color: 'white', 
+                '& .MuiOutlinedInput-notchedOutline': { 
+                  borderColor: 'white', 
+                },
+                '&:hover .MuiOutlinedInput-notchedOutline': {
+                  borderColor: 'white', 
+                },
+                '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                  borderColor: 'white', 
+                },
+                '& .MuiSvgIcon-root': { 
+                  color: '#63A4FF', 
+                },
+              }}
+            >
+              <MenuItem value="">
+                <em>Select a format type</em>
+              </MenuItem>
+              <MenuItem value="csv">CSV</MenuItem>
+              <MenuItem value="json">JSON</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} sx={{ color: '#63A4FF' }}>Cancel</Button>
+          <Button
+            onClick={handleExport}
+            variant="contained"
+            
+            sx={{
+                bgcolor: '#376BD2',
+                color: 'white',
+                marginRight: "10px",
+                '&:hover': { bgcolor: '#173B6C' }
+            }}
+          >
+            Export
+          </Button>
+        </DialogActions>
+      </Dialog>
+          
+          </Box>}
 
       <Box sx={{ width: "180px", ...row, height: "40px" }}>
         <ItemSwitcher />
@@ -73,7 +190,7 @@ const Header = () => {
           rel="noopener noreferrer"
           sx={{
             alignItems: "center",
-            color: "#000000",
+            color: "white",
             mr: 1,
             "&:hover": {
               backgroundColor: "rgba(28, 72, 134, 0.04)",
